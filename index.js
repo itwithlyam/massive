faaa0111302e7def86b0db3df497e3d6b474e418
itwithlyam <64089164+itwithlyam@users.noreply.github.com>
2020-10-24 14:43:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ module.exports = function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Massive wants a string!");
     return string.replace(/\s/g, "");
 };
+module.exports = function upper(string) {
+    if (typeof string !== "string") throw new TypeError("Massive wants a string!");
+    return string.toUpperCase();
+};
